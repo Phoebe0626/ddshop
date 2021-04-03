7b2b6ebcb8a83718f9806db1d9d8192db3c1e8b6
phoebe <506014196@qq.com>
2021-04-03 15:55:41 +0800

    完成修改生日功能

--- src/views/My/Children/UserCenter.vue
+++ src/views/My/Children/UserCenter.vue
@@ -9,12 +9,40 @@
     <!-- 个人资料 -->
     <van-cell-group style="margin-top: 0.2rem">
       <van-cell is-link title="昵称" :value="userName" @click="$router.push('/dashboard/my/userCenter/changeName')"></van-cell>
-      <van-cell is-link title="性别" :value="sex"></van-cell>
-      <van-cell is-link title="生日" :value="userBirth ? userBirth : '未填写'"></van-cell>
+      <van-cell is-link title="性别" :value="sex" @click="showSelectSex = true"></van-cell>
+      <van-cell is-link title="生日" :value="userBirth | formatDate" @click="showSelectDate = true"></van-cell>
       <van-cell title="手机号" :value="userPhone.substr(0, 3) + '****' + userPhone.substr(7)"></van-cell>
     </van-cell-group>
     <!-- 退出登录 -->
     <van-button size="large" style="margin-top: 0.4rem" type="default" block>退出登录</van-button>
+    <!-- 选择性别 -->
+    <van-action-sheet v-model="showSelectSex">
+      <van-radio-group v-model="sexRadio">
+        <van-cell-group style="margin: 1rem 0;">
+          <van-cell title="美女" clickable @click="hSetUserSex(1)">
+            <template #right-icon>
+              <van-radio checked-color="#07c160" :name="1" />
+            </template>
+          </van-cell>
+          <van-cell title="帅哥" clickable @click="hSetUserSex(2)">
+            <template #right-icon>
+              <van-radio checked-color="#07c160" :name="2" />
+            </template>
+          </van-cell>
+        </van-cell-group>
+      </van-radio-group>
+    </van-action-sheet>
+    <!-- 生日弹框 -->
+    <van-action-sheet v-model="showSelectDate">
+      <van-datetime-picker
+        v-model="date"
+        type="date"
+        :min-date="minDate"
+        :max-date="maxDate"
+        @confirm="hSetUserBirth"
+        @cancel="showSelectDate = false"
+      />
+    </van-action-sheet>
     <transition name="slideUpDown" mode="out-in">
       <router-view></router-view>
     </transition>
@@ -22,10 +50,15 @@
 </template>
 
 <script>
-import { NavBar, Cell, CellGroup, Button } from 'vant'
-import { mapGetters } from 'vuex'
+import { NavBar, Cell, CellGroup, Button, ActionSheet, RadioGroup, Radio, Toast, DatetimePicker } from 'vant'
+import { mapGetters, mapMutations } from 'vuex'
 export default {
   components: {
+    [DatetimePicker.name]: DatetimePicker,
+    [Toast.name]: Toast,
+    [RadioGroup.name]: RadioGroup,
+    [Radio.name]: Radio,
+    [ActionSheet.name]: ActionSheet,
     [Button.name]: Button,
     [Cell.name]: Cell,
     [CellGroup.name]: CellGroup,
@@ -42,6 +75,54 @@ export default {
       } else {
         return '未填写'
       }
+    }
+  },
+  data () {
+    return {
+      minDate: new Date('1960-01-01'),
+      maxDate: new Date('2020-01-01'),
+      showSelectDate: false, // 选择生日弹框
+      date: new Date('1995-01-01'), // 生日
+      sexRadio: '', // 性别
+      showSelectSex: false // 显示性别弹框
+    }
+  },
+  created () {
+    this.sexRadio = this.userSex
+    this.date = this.userBirth ? this.userBirth : new Date('1995-01-01')
+  },
+  methods: {
+    ...mapMutations(['setUserSex', 'setUserBirth']),
+    hSetUserBirth (val) {
+      console.log(val)
+      this.date = val
+      this.setUserBirth(val)
+      this.showSelectDate = false
+      Toast({
+        message: '保存成功',
+        duration: 800
+      })
+    },
+    hSetUserSex (sex) {
+      this.sexRadio = sex
+      this.setUserSex(sex)
+      this.showSelectSex = false
+      Toast({
+        message: '保存成功',
+        duration: 800
+      })
+    }
+  },
+  filters: {
+    formatDate (time) {
+      const date = new Date(time)
+      const y = date.getFullYear()
+      const m = addZero(date.getMonth() + 1)
+      const d = addZero(date.getDate())
+      function addZero (val) {
+        return val < 10 ? '0' + val : val
+      }
+      return y + '-' + m + '-' + d
     }
   }
 }
